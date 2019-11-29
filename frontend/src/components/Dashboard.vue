36988aa46aa2d1a13c25d6a05d4aa64c7084579f
Martijn Koch <martijn_Waterreus@hotmail.com>
2019-11-29 13:51:51 +0100

    Additional pages added

---
+++ frontend/src/components/Dashboard.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container-fluid header-container">
+    <vue-headful
+    title="Dashboard"
+    description="Description from vue-headful"
+    />
+<b-container class="bv-example-row mt-5">
+<h1>test</h1>
+<b-row>
+</b-row>
+</b-container>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../assets/css/variables.scss";
+
+ h1 {
+     color: $gh-purple;
+ }
+</style>
---
+++ frontend/src/components/Profile.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+<vue-headful
+    title="Profile"
+    description="Description from vue-headful"
+    />
+<h1>Profile page</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../assets/css/variables.scss";
+
+ h1 {
+     color: $gh-purple;
+ }
+</style>
