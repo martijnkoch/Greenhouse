3fd056b13c8e03a99a66b335766042e090faf253
Martijn Koch <martijn_Waterreus@hotmail.com>
2019-12-04 17:07:30 +0100

    Api routes

---
+++ backend/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->group(function () {
+    
+});
+
+//User controller
+Route::post('/register', 'Api\AuthController@register');
+Route::post('/login', 'Api\AuthController@login');
+
+//Game controller
+Route::get('/games', 'Api\GameController@index');
+Route::post('/addgame', 'Api\GameController@store');
